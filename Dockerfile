c8d7dc4883fa593410acb35f945a4fb93f5ae930
zhongpengqun <zhongpengqun2022@gmail.com>
2023-04-03 16:37:52 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM python:2.7
 COPY . /
 WORKDIR /
-RUN pip install mkdocs
+RUN pip install --default-timeout=100 mkdocs
+#EXPOSE 7777
+#CMD [ "mkdocs", "serve"， "-a",  "0.0.0.0:7777" ]
 EXPOSE 8080
-CMD ["mkdocs", "serve"]
+CMD [ "mkdocs", "serve" ]
--- run-this-blog.sh
+++ run-this-blog.sh
@@ -1,8 +1,11 @@
 # /bin/sh
 
+# todo, 有点问题，run不起来
 # Docker run it, it is the best! refer to https://medium.com/@edsonalcalamx/running-nethereum-docs-with-docker-5b8a4c25d42f
-docker build -t blog .
-docker run -d blog
+# docker rmi -f blog
+# ocker rm -f $(docker ps |grep mkdocs |awk '{ print $1 }')
+# docker build -t blog .
+# docker run -d blog
 
-#mkdocs serve -a 0.0.0.0:7777
+mkdocs serve -a 0.0.0.0:7777
 
