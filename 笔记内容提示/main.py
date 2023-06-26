43d063ada00ff896f6114987ced796e162eb7a38
Vincent Zhong <vzhong@vmware.com>
2023-06-26 18:37:52 +0000

    oo

--- 笔记内容提示/main.py
+++ 笔记内容提示/main.py
@@ -37,19 +37,32 @@ def get_all_lines():
 
 get_all_lines()
 
+def print_keyword_related_note(keyword):
+    for _i, _line in enumerate(all_lines):
+        if keyword in _line.lower():
+            all_lines[_i] = all_lines[_i].replace(keyword, f'\033[92m{keyword}\033[0m')
+            print('\n'.join(all_lines[_i - RELATED_LINES_UP_COUNT : _i + RELATED_LINES_DOWN_COUNT]))
+            # print(_line)
+            # print(_i)
+            # print('\n\n')
+            print('-'*50)
+
 # all_lines_lower = [x.lower() for x in all_lines]
 
 test_str = '扛'
+test_str = read_from_clipboard()
 
-for _i, _line in enumerate(all_lines):
-    if test_str in _line.lower():
-        all_lines[_i] = all_lines[_i].replace(test_str, f'\033[92m{test_str}\033[0m')
-        print('\n'.join(all_lines[_i - RELATED_LINES_UP_COUNT : _i + RELATED_LINES_DOWN_COUNT]))
-        # print(_line)
-        # print(_i)
-        # print('\n\n')
-        print('-'*50)
-# if __name__ == "__main__":
+current_keyword = ''
+
+while True:
+    clipboard_content = read_from_clipboard()
+    if clipboard_content != current_keyword:
+        print_keyword_related_note(clipboard_content)
+        current_keyword = clipboard_content
+    time.sleep(2)
+
+
+    # if __name__ == "__main__":
 #     while 1:
 #         clipboard_text = read_from_clipboard()
 
