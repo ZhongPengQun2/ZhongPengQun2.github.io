bb19860370c97945a56adc4faf19a4f109ce387d
Vincent Zhong <vzhong@vmware.com>
2023-06-26 17:35:41 +0000

    x

--- 笔记内容提示/main.py
+++ 笔记内容提示/main.py
@@ -13,7 +13,9 @@ import re
 import glob
 import time
 
-all_docs = []
+all_lines = []
+# all_lines_lower = []
+
 current_clipboard_content = ""
 
 def read_from_clipboard():
@@ -21,25 +23,24 @@ def read_from_clipboard():
     return subprocess.check_output(
         'pbpaste', env={'LANG': 'en_US.UTF-8'}).decode('utf-8')
 
-def show_matched():
-    print('-----1')
-    for doc_postfix in doc_postfixs:
-        print('---------d')
-        for doc in glob.glob("{}/*.{}".format(docs_path, doc_postfix)):
-            print(doc)
-            with open(doc, "r") as f:
-                # all_docs.append('\n'.join(f.readlines()))
-                for x in f.readlines():
-                    print(x)
-                    print('-------------')
-                # todo test
-                break
+def get_all_lines():
+    for path, subdirs, files in os.walk(docs_path):
+        for name in files:
+            if not name.endswith(tuple(doc_postfixs)):
+                continue
+            with open(f'{path}/{name}') as f:
+                for _line in f.readlines():
+                    all_lines.append(_line)
 
-    all_docs_text = "\n\n".join(all_docs)
-    return all_docs_text
+get_all_lines()
 
-all_docs_text = show_matched()
-print(all_docs_text)
+# all_lines_lower = [x.lower() for x in all_lines]
+
+test_str = 'python'
+
+for _line in all_lines_lower:
+    if test_str in _line.lower():
+        print(_line)
 # if __name__ == "__main__":
 #     while 1:
 #         clipboard_text = read_from_clipboard()
